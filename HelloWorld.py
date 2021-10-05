303fdf7c8a9939de8c7001b02e6194ea7de92b49
dasSiva <91928257+dasSiva@users.noreply.github.com>
2021-10-06 07:02:49 +1100

    practice

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,40 @@
-print("Hello world") #comment#
+print("Hello world")  # comment#
+
+#  This is a sample addition
+
+
+sum = 0
+a = 10
+b = 20
+if a < b:
+    sum = a+b
+    print(sum)
+
+
+# Types cast
+x = str(3)
+y = int(3.5)
+z = float(3)
+
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# cases
+a = "Siva"
+A = "Das"
+print(a)
+print(A)
+
+
+# Illegal var name
+
+myvar = "Das"
+my var = "Das"  # illegal
+print(myvar)
+print(my var)  # error case
