91c525cca9cf8a3ecdd03605191de616c31c3ab8
dasSiva <91928257+dasSiva@users.noreply.github.com>
2021-10-10 11:19:50 +1100

    Create list.py

---
+++ list.py
@@ -0,0 +1,23 @@
+
+# list
+thislist = ["apple", "banana", "cherry"]
+intlist = [1, 2, 3, 4]
+fltlist = [1.2, 3.4, 2.3]
+print(len(thislist[1]))
+
+print(intlist)
+print(fltlist)
+
+print(type(intlist))
+
+
+class zoo():
+    def animals():
+        thislist = ["dog", "cat", "cow"]
+        print(len(thislist))
+
+    animals()
+
+
+obj = zoo()
+list()
