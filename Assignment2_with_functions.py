fc8b229159f4b6c3f095643a76748516995f0ec1
dasSiva <91928257+dasSiva@users.noreply.github.com>
2021-10-19 10:50:59 +1100

    resubmitted on review

--- Assignment2_with_functions.py
+++ Assignment2_with_functions.py
@@ -8,9 +8,7 @@
 import os
 
 # global variable assigned with characters that will be used to encrypt and decrypt password
-encrptKey = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@\'#,\"$!&\\"
-
-# Getting username
+encrptKey = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@\'#,\"$!&\\~%^*()_+=-?/><;:|.{}[]"
 name = input("Welcome to Apps2U. Please enter your name: ")
 
 
@@ -25,15 +23,15 @@ else:
 
 # defining the function that saves username,password and url
 def add_details():
-    # Requesting user for username,password and url
+
     username = input("Enter username: ")
     password = input("Enter password: ")
     url = input("Enter url: ")
 
-    # Encrypting password
-    secret = "".join([encrptKey[(encrptKey.find(character)+3) % 71]
+    # Encrypting password- For every character in password, find the character in the "encryptKey" string and replace it with the character which is located at the 3rd position from that character
+    secret = "".join([encrptKey[(encrptKey.find(character)+3) % 93]
                       for character in password])
-    # Opening file to append
+
     f = open(name+".txt", "a")
 
     # Saving to file separated by tab and |
@@ -43,21 +41,21 @@ def add_details():
 
 # defining the function that reads file and display content
 def view_details():
-    # Opening file to read
+
     f = open(name+".txt", "r")
 
     # Readlines reads all lines and stores as list in rows.
     rows = f.readlines()
 
-    # Closing file after read
     f.close()
+
     #  print header
     print("==========================================")
     # Left aligning
     print('{:12}  {:12}  {:12}'.format(
         "Username", "Password", "url"))
     print("==========================================")
-    # Going through each items in a list
+
     for everyrow in rows:
         # Spliting everyrow using tab and | and storing in local variable
         column = everyrow.split("\t|")
@@ -65,40 +63,34 @@ def view_details():
         # Password was stored on index 1. Assigning that to secret_pswd
         secret_pswd = column[1]
 
-        # Decrypt the password
+        # Decrypt the password- For every character in the encrypted password, find the character in the "encryptKey" string and replace it with the character which is located 3rd positions to the left
 
-        decrpted_pswd = "".join([encrptKey[(encrptKey.find(character)-3) % 71]
+        decrpted_pswd = "".join([encrptKey[(encrptKey.find(character)-3) % 93]
                                  for character in secret_pswd])
 
-        # Print username, decrypted password and url formating to be left aligned
         print('{:12}  {:12}  {:12}'.format(
             column[0], decrpted_pswd, column[2]))
         print("----------------------------------------")
 
 
-# Assigning empty string to choice
 choice = ""
 while choice != 'Q':
-    # Displaying the options menu
+
     print("Enter your selection from the menu")
     print("Enter A for adding details")
     print("Enter V for viewing the details")
     print("Enter Q for exiting the applciation")
 
-  # Requesting input from user
     choice = input("Enter your choice: ")
 
-   # Checking if choice is A
     if choice == 'A':
         add_details()
 
-    # Checking if choice is V
     elif choice == 'V':
         view_details()
 
-    # Exit application if any other choice
     elif choice != 'Q':
         print("Invalid choice")
 
-# Exiting the application when choice is Q
+
 print("Exiting application")
